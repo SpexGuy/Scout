fc94d60066692e6e8dc496bb854039bb66af3311
Martin Wickham <spexguy070@gmail.com>
2015-11-04 12:37:11 -0600

    Add a simple problem for testing

--- scout.py
+++ scout.py
@@ -20,11 +20,32 @@ class Problem:
         return "BadProblem"
 
 
+class SquareProblem(Problem):
+    def __init__(self, size):
+        self.size = size
+
+    def getStartState(self):
+        return (0, 0)
+
+    def getEndState(self):
+        return (self.size, self.size)
+
+    def isValidState(self, state):
+        return 0 <= state[0] <= self.size and
+               0 <= state[1] <= self.size
+
+    def getSuccessors(self, state):
+        return [(state[0]+dx, state[1]+dy) for (dx, dy) in [(1, 0), (0, 1), (-1, 0), (0, -1)]]
+
+    def getStringRepr(self, state):
+        return "(%d, %d)" % state
+
+
 def search(problem):
     print "Searching..."
 
 
 
 if (__name__ == '__main__'):
-    problem = Problem();
+    problem = SquareProblem(2);
     search(problem)
